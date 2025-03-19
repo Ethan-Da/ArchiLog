b8e8111902bcdeb31b045ebfb4a8bded82308597
Ed <ethan.dangeul@gmail.com>
2025-03-19 15:29:47 +0100

    Auth et Autorisations

--- src/archilog/views/__init__.py
+++ src/archilog/views/__init__.py
@@ -1,6 +1,6 @@
-import os
+
 from flask import Flask
-import logging
+
 
 def create_app():
     app = Flask(__name__)
--- src/archilog/views/web.py
+++ src/archilog/views/web.py
@@ -13,10 +13,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField
 from wtforms.validators import DataRequired
 
-class UpdateForm(FlaskForm):
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
 
+class UpdateForm(FlaskForm):
     name = StringField("name", validators=[])
-    price = FloatField("price", validators=[])
+    price = FloatField("price", validators=[], default=None)
     category = StringField("category", validators=[])
 
 class AddForm(FlaskForm):
@@ -27,29 +30,42 @@ class AddForm(FlaskForm):
 web_ui = Blueprint("web_ui", __name__, url_prefix="/")
 api = Blueprint("api", __name__, url_prefix="/api")
 
+auth = HTTPBasicAuth()
+users = {
+    "Ethan": [generate_password_hash("admin"), ["admin", "user"]],
+    "max": [generate_password_hash("max"), ["user"]]
+}
+
+
+#==============ROUTES WEBUI BLUEPRINT================#
 
 @web_ui.route("/")
+@auth.login_required
 def show():
     return render_template("web_ui.html", list=get_all_entries(), addform=AddForm())
 
 @web_ui.route("/delete/<id>")
+@auth.login_required(role="admin")
 def delete(id:str = None):
     delete_entry(uuid.UUID(id))
     return render_template("web_ui.html", list=get_all_entries(), addform=AddForm())
 
 @web_ui.route("/add", methods=['POST'])
+@auth.login_required(role="admin")
 def add():
     form = AddForm()
     if form.validate_on_submit():
         create_entry(form.name.data, form.price.data, form.category.data)
-    return render_template("web_ui.html", list=get_all_entries(), addform=form)
+    return render_template("web_ui.html", list=get_all_entries(), addform=AddForm())
 
 @web_ui.route("/update_page/<id>", methods=["GET"])
+@auth.login_required(role="admin")
 def update_page(id:str = None):
     select = get_entry(uuid.UUID(id))
     return render_template("update.html", ligne=select, updateform=UpdateForm())
 
 @web_ui.route("/do_update/<id>", methods=["POST"])
+@auth.login_required(role="admin")
 def do_update(id:str = None):
     form = UpdateForm()
     if form.validate_on_submit():
@@ -58,6 +74,7 @@ def do_update(id:str = None):
     return render_template("web_ui.html", list=get_all_entries(), addform=AddForm())
 
 @web_ui.route("/importcsv", methods=['POST'])
+@auth.login_required(role="admin")
 def importcsv():
     file = request.files['csv-file']
     filestream = io.StringIO(file.stream.read().decode('utf8'))
@@ -65,11 +82,14 @@ def importcsv():
     return render_template("web_ui.html", list=get_all_entries())
 
 @web_ui.route("/exportcsv", methods=['POST'])
+@auth.login_required
 def exportcsv():
     output = export_to_csv()
     response = Response(output.getvalue(), content_type='text/csv')
     response.headers['Content-Disposition'] = 'attachment; filename="export.csv"'
     return response
+
+ #==============ERROR================#
 
 @web_ui.errorhandler(IntegrityError)
 def handle_internal_error(error):
@@ -81,3 +101,16 @@ def handle_internal_error(error):
 def handle_internal_error(error):
     flash("Erreur interne du serveur", "error")
     return redirect("/")
+
+
+ #==============AUTHENTIFICATION================#
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username)[0], password):
+        return username
+
+@auth.get_user_roles
+def get_user_roles(username):
+    return users.get(username)[1]
